96e2df9afc377d10dd9aea481201e777592af2fb
Ante Gazibarić <Ante.Gazibaric@fer.hr>
2019-05-15 16:24:15 +0200

    :(

--- NBA.R
+++ NBA.R
@@ -9,3 +9,29 @@ upperLimit = q[3] + 1.5 * i
 lowerLimit = q[1] - 1.5 * i
 tallPlayers = playersData[playersData$height > upperLimit, ]
 shortPlayers = playersData[playersData$height < lowerLimit, ]
+player = player_data[player_data$position == "C" & asInch(as.character(player_data$height)) > 84, ]
+as = asInch(as.character(player_data$height))
+chars = as.character(player_data$height)
+l = asInch(chars)
+
+h = levels(player$position)
+a = player_data$height[1]
+tokens <- as.numeric(unlist(strsplit(as.character(a), "-")))
+
+
+getVectorInch <- function(vec) {
+  
+}
+
+asInch <- function(height) {
+  if (is.vector(height)) {
+    result <- numeric()
+    for (i in 1:length(height)) {
+      result <- c(result, asInch(height[i]))
+    } 
+    return(result)
+  }#
+  tokens <- as.numeric(unlist(strsplit(height, "-")))
+  return(12 * tokens[1] + tokens[2])
+}
+asInch("7-0")
