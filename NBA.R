2e7e079758d744571ec5c2d8d0fb27041f39e868
Ante Gazibarić <Ante.Gazibaric@fer.hr>
2019-05-15 15:21:12 +0200

    Initial commit

---
+++ NBA.R
@@ -0,0 +1,11 @@
+setwd("C:/Users/ante1/Documents/Development/R/Projekt/NBA")
+playersData <- read.csv(file="./Data/nba-players-stats/Players.csv", header=TRUE, sep=",")
+player_data <- read.csv(file="./Data/nba-players-stats/player_data.csv", header=TRUE, sep=",")
+seasonsStats <- read.csv(file="./Data/nba-players-stats/Seasons_Stats.csv", header=TRUE, sep=",")
+
+q = quantile(playersData$height, probs = c(0.25, 0.5, 0.75), na.rm = TRUE)
+i = IQR(playersData$height, na.rm = TRUE)
+upperLimit = q[3] + 1.5 * i
+lowerLimit = q[1] - 1.5 * i
+tallPlayers = playersData[playersData$height > upperLimit, ]
+shortPlayers = playersData[playersData$height < lowerLimit, ]
